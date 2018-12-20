c7874fe0fae0491e1964c4afc9cb57ea31ce38f8
George Butter <threeninenineone@gmail.com>
2018-12-20 13:26:43 +1100

    version bump

--- limelight.js
+++ limelight.js
@@ -1,7 +1,7 @@
 /* ===================================================================================== @preserve =
 
 Limelight
-version v2.2.0
+version v2.2.3
 Author: George Butter
 https://github.com/ButsAndCats/limelight
 ISC License
